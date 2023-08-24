5256debc4a8d493e642b6cc89f3d5fb353f44c69
Luis Gallo <l.gallogomez@gmail.com>
2023-08-24 14:33:22 -0500

    spaces scenario

--- shell.c
+++ shell.c
@@ -13,7 +13,7 @@ int main(void)
 	ssize_t line_len = 0;
 	size_t arg_line_len = 0;
 	char *line = NULL, *token, *str[5];
-	const char delimiter[] = " \n\t\0    ";
+	const char delimiter[] = " \n\t\0";
 	int i = 1;
 
 	while (1)
@@ -23,7 +23,7 @@ int main(void)
 		if (isatty(STDIN_FILENO))
 			write(1, "($) ", 5);
 		line_len = getline(&line, &arg_line_len, stdin);
-		if (line_len == EOF || (strcmp(line, "exit\n") == 0))
+		if (line_len == EOF || (strcmp(line, "exit\n") == 0) || (strcmp(line, " \n") == 0) || (strcmp(line, "     \n") == 0))
 		{
 			free(line);
 			exit(0);
